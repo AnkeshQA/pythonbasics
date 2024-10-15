1a013d27d17f768f2312f614e5599b867c33a501
ankesh <ashikhar6@gmail.com>
2024-10-16 02:36:52 +0530

    end to end api automation via tutorial Update :- global configurations in API

--- postapi.py
+++ postapi.py
@@ -4,12 +4,28 @@
 # refer the document https://requests.readthedocs.io/en/latest/
 import requests
 from payload import *
+
+
 #created a function payload.py and imported the details so that unique id will be created & automatically it will updated and deleted without any failure
 
 from apirequest import response
+# import configparser --> this is moved to configurations.py file
 
-add_book_response = requests.post(
-    'http://216.10.245.166/Library/Addbook.php',
+from utilities.configurations import *
+config = getconfig()
+
+#date :- 16/10/2024
+#create an utilities file and add end point in properties file which has .ini extension
+# then use import configparser and then follow below steps config = configparser.ConfigParser() & config.read('utilities/properties.ini') (moved to configuration file)
+# config = configparser.ConfigParser()
+# use the below property to read the path of the file
+# use read method here
+# config.read('utilities/properties.ini')
+
+#Next step :- move from (import configparser till config.read('utilities/properties.ini') to configuration file
+
+# (config['API']['endpoint']+ this will read endpoint from properties.ini file
+add_book_response = requests.post(config['API']['endpoint']+'/Library/Addbook.php',
     json=addBookPayload("fefrewe"),  # Call the function and use its return value directly
     headers={"Content-Type": "application/json"}
 )
@@ -20,7 +36,7 @@ print(type(add_book_response.json()))
 bookID = response_json['ID']
 
 #delete the book
-response_delete_book = requests.post('http://216.10.245.166/Library/DeleteBook.php',json={
+response_delete_book = requests.post(config['API']['endpoint']+'/Library/DeleteBook.php',json={
     "ID" : bookID
 },headers={"Content-Type":"application/json"},)
 assert response_delete_book.status_code == 200
